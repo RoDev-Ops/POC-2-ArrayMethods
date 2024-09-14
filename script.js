7e05ab21432012d715c87eda9e9cc6513ddfc0d6
TshadowBR <rodrigomello789010@gmail.com>
2024-09-14 20:21:40 -0300

    finishing POC

--- script.js
+++ script.js
@@ -16,18 +16,10 @@ products = [
   },
   {
     name: "Sorvete",
-    quantity:10,
-    price: 31.3 
+    quantity: 10,
+    price: 31.3,
   },
 ];
-// Esqueci como é faturamento de inglês :P
-function getFaturamento(acc, prod) {
-    return acc + (prod.price * prod.quantity);
-  }
-  
-function lastStock(stock){
-    return stock.quantity <= 15
-}
 
 
 
@@ -38,38 +30,81 @@ window.onload = () => {
             <tr>
                 <td>${products[i].name}</td>
                 <td>${products[i].quantity}</td>
-                <td>${products[i].price}</td>
+                <td>${products[i].price.toFixed(2)}</td>
             </tr>`;
+  }
+  function getFaturamento(acc, prod) {
+    return acc + prod.price * prod.quantity;
   }
   const reduceTable = document.getElementById("reduceTable");
   reduceTable.innerHTML = `<tr> 
-        <td>${products.reduce(getFaturamento,0)}</td>
+        <td>${products.reduce(getFaturamento, 0)}</td>
     </tr>`;
-  reduceTable.style.textAlign ="center";
+  reduceTable.style.textAlign = "center";
 
   const filterTable = document.getElementById("filterTable");
   let productsFiltered = products.slice(0);
+  
+  function lastStock(stock) {
+    return stock.quantity <= 15;
+  }
   productsFiltered = products.filter(lastStock);
+  
 
-  productsFiltered.forEach(product =>{
+  productsFiltered.forEach((product) => {
     filterTable.innerHTML += `<tr> 
         <td>${product.name}</td>
         <td>${product.quantity}</td>
-        <td>${product.price}</td>
+        <td>${product.price.toFixed(2)}</td>
     </tr>`;
-  })
-
+  });
+  filterTable.style.textAlign = "center";
   const sortProducts = document.getElementById("sortTable");
   let productsSorted = products.slice(0);
-  productsSorted.sort((a,b) =>{
-    return b.price-a.price
-  })
+  productsSorted.sort((a, b) => {
+    return b.price - a.price;
+  });
 
-  productsSorted.forEach(product=>{
+  productsSorted.forEach((product) => {
     sortProducts.innerHTML += `<tr> 
         <td>${product.name}</td>
         <td>${product.quantity}</td>
-        <td>${product.price}</td>
+        <td>${product.price.toFixed(2)}</td>
     </tr>`;
-  })
+  });
+  sortProducts.style.textAlign = "center";
+
+  newHeader = document.getElementById("newHeader");
+  spreadTable = document.getElementById("spreadTable");
+  spreadTable.style.textAlign = "center";
+  let auxProducts = products.slice(0);
+  spreadProducts = auxProducts.forEach((product) => {
+    product = {
+      ...product,
+      discount: product.price * 0.5,
+    };
+
+    spreadTable.innerHTML += `<tr> 
+        <td>${product.name}</td>
+        <td>${product.quantity}</td>
+        <td>${product.price.toFixed(2)}</td>
+        <td>${product.discount.toFixed(2)}</td>
+    </tr>`;
+  });
+
+  let newPriceTable = document.getElementById("priceAdjustment");
+  const discountedProducts = products.map((product) => {
+    return {
+      ...product,
+      price: (product.price * 0.5).toFixed(2),
+    };
+  });
+  discountedProducts.forEach((product) => {
+    newPriceTable.innerHTML += `<tr> 
+      <td>${product.name}</td>
+      <td>${product.quantity}</td>
+      <td>${product.price}</td>
+  </tr>`;
+  });
+  newPriceTable.style.textAlign = "center";
 };
